e4137d1180394a1c78049bbc590da9786ef92994
yevhenii-kasian <mc.zkasyan@gmail.com>
2013-11-06 14:33:54 +0200

    xaml

---
+++ Searcher/SearcherWPF/ISite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearcherWPF
+{
+    interface ISite
+    {
+        string ReturnFilter();
+        string ReturnName();
+        string ReturnUrl();
+        List<string> ReturnTitleList();
+    }
+}
